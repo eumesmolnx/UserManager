d2054895dd2bf76565d4a6183515ecd708999d6d
eumesmolnx <eumesmolnx@yahoo.com>
2022-10-06 02:34:02 -0300

    Run Django migrate to create initial tables and custom User structure

--- usermanager/usermanager/settings.py
+++ usermanager/usermanager/settings.py
@@ -91,6 +91,9 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
+AUTHENTICATION_BACKENDS = ['django.contrib.auth.backends.ModelBackend']
+AUTH_USER_MODEL = 'app.User'
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
