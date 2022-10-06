4a5dd0f6071b644af551efaff14e3e44d8690f42
eumesmolnx <eumesmolnx@yahoo.com>
2022-10-06 02:36:56 -0300

    Create View/Viewset of User and return Authentication Token + adjust Role permission in User viewset as IsAdmin

--- usermanager/app/views.py
+++ usermanager/app/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
 
 # Create your views here.
+from rest_framework import viewsets
+from .serializers import UserSerializer
+from .models import User
+from .serializers import MyTokenObtainPairSerializer
+from rest_framework import permissions
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+# Custom permissions
+class IsAdmin(permissions.BasePermission):
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        else:
+            return request.user.role == 'ADMIN'
+
+# Create your views here.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = (IsAdmin,)
+
+class MyObtainTokenPairView(TokenObtainPairView):
+    permission_classes = (permissions.AllowAny,)
+    serializer_class = MyTokenObtainPairSerializer
