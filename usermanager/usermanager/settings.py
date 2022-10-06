fcf6bfaeb314fa9de49e3d794845a055b4626c77
eumesmolnx <eumesmolnx@yahoo.com>
2022-10-06 01:17:20 -0300

    Update Django settings to add App, Rest Framework, Django Filters and Postgres Database Connection

--- usermanager/usermanager/settings.py
+++ usermanager/usermanager/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_filters',
+    'rest_framework',
+    'app'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +78,12 @@ WSGI_APPLICATION = 'usermanager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'igsdb',
+        'USER': 'igsuser',
+        'PASSWORD': 'igspass',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
